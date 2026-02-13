53a99ac135446b1fd93d34e68da1eaef9d96e7c0
arnaldo Mendez <arnaldomendez5@gmail.com>
2026-02-13 08:17:01 -0300

    nuevo diseño

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('btnMensaje').addEventListener('click', function() {
+    const mensaje = document.getElementById('mensajeOculto');
+    mensaje.innerHTML = "<strong>¡Éxito en tus estudios de Química, Arnaldo!</strong> 🧪✨";
+    mensaje.style.color = "#2c3e50";
+});
