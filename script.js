ec2651c9545aa7ac1ff5c0ef18e69b1f294c0c1b
arnaldo Mendez <arnaldomendez5@gmail.com>
2026-02-13 09:00:54 -0300

    Atlas visual con Bioseguridad y Guia 1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('btnMensaje').addEventListener('click', function() {
     const mensaje = document.getElementById('mensajeOculto');
-    mensaje.innerHTML = "<strong>¡Éxito en tus estudios de Química, Arnaldo!</strong> 🧪✨";
+    mensaje.innerHTML = "<strong>¡Éxito en tus estudios de Química, RECUERDA TU NOMBRE Y APELLIDO!</strong> 🧪✨";
     mensaje.style.color = "#2c3e50";
 });
