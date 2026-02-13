1e92390b2da5d20a08062cd005345c6c7e60c2e6
arnaldo Mendez <arnaldomendez5@gmail.com>
2026-02-13 09:41:39 -0300

    Version final

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btnMensaje').addEventListener('click', function() {
-    const mensaje = document.getElementById('mensajeOculto');
-    mensaje.innerHTML = "<strong>¡Éxito en tus estudios de Química, RECUERDA TU NOMBRE Y APELLIDO!</strong> 🧪✨";
-    mensaje.style.color = "#2c3e50";
-});
